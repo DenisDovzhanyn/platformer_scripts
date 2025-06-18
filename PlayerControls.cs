28fdee051db0b497b066d86bfc63cf45650115b1
Denis <dovzhanyndenis03@gmail.com>
2025-06-18 14:25:56 -0400

    feat: movement feeling better

--- PlayerControls.cs
+++ PlayerControls.cs
@@ -7,47 +7,47 @@ public class NewBehaviourScript : MonoBehaviour
     private Rigidbody rb;
     private InputAction moveAction;
     private InputAction jumpAction;
-    private InputAction sprintAction;
+    private InputAction lookAction;
     private Camera camera;
     public float moveSpeed;
     private Vector2 moveDir;
+    private Vector2 lookInput;
     public float jumpForce;
-    public float fallRate;
+    public float fallMultiplier;
     private bool jump;
     private bool isGrounded;
     private bool hasDoubleJumped;
     private bool canJump;
+    private float pitch; // we keep our own pitch 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        pitch = 0f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
         canJump = true;
         jump = false;
         isGrounded = true;
         hasDoubleJumped = false;
+
         camera = GetComponentInChildren<Camera>();
         rb = GetComponent<Rigidbody>();
 
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
-        sprintAction = InputSystem.actions.FindAction("Sprint");
+        lookAction = InputSystem.actions.FindAction("Look");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        GatherInput();
+        HandleCameraPitch();
+
+        // because camera is attached to player and its rotation is relative to it, we rotate the player left/right
+        rb.transform.Rotate(Vector3.up * lookInput.x);
         
-        moveDir = moveAction.ReadValue<Vector2>() * moveSpeed;
-
-        if (sprintAction.IsPressed())
-        {
-            camera.fieldOfView = 90;
-        }
-        else if (camera.fieldOfView != 60)
-        {
-            camera.fieldOfView = 60;
-        }
-
-
         if (jumpAction.IsPressed() && canJump)
         {
             Debug.Log("jump action started");
@@ -62,25 +62,16 @@ public class NewBehaviourScript : MonoBehaviour
 
     void FixedUpdate()
     {
-
-        rb.linearVelocity = new Vector3(moveDir.x, rb.linearVelocity.y, moveDir.y);
-
-        if (jump)
-        {
-            Debug.Log("adding jump force");
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
-            //rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            jump = false;
-        }
-
-        if (rb.linearVelocity.y < 0)
-        {
-            //rb.linearVelocity = new Vector3(rb.linearVelocity.x, fallRate , rb.linearVelocity.z);
-            rb.linearVelocity += Vector3.up * (fallRate * Time.fixedDeltaTime);
-            // if this doesnt work we can set it back to (physics.gravity.y * fallrate * time.timeimttietiteimteit)
-        }
+        // var move = new Vector3(moveDir.x, rb.linearVelocity.y, moveDir.y);
+        HandleMovement();
+        HandleJump();
+        HandleFall();
     }
 
+    void LateUpdate()
+    {
+        camera.transform.localRotation = Quaternion.Euler(pitch, 0,0);
+    }
 
     void OnCollisionEnter(Collision collision)
     {
@@ -99,6 +90,69 @@ public class NewBehaviourScript : MonoBehaviour
         {
             Debug.Log("Left floor");
             isGrounded = false;
+        }
+    }
+
+    void HandleCameraPitch()
+    {
+        // even though it hurts my head, we subtract because in 'eular angles' a positive pitch makes us point down on the x axis
+        pitch -= lookInput.y ;
+        pitch = Mathf.Clamp(pitch, -90, 90);
+    }
+
+    void GatherInput()
+    {
+        moveDir = moveAction.ReadValue<Vector2>() * moveSpeed;
+        lookInput = lookAction.ReadValue<Vector2>();
+    }
+
+    void HandleMovement()
+    {
+        Vector3 forward = transform.forward;
+        Vector3 right = transform.right;
+
+        // makes player move either forward or backward depending on y input (W or S);
+        forward *= moveDir.y;
+        // makes player move side to side relative to way they are facing depending on x input (A or D);
+        right *= moveDir.x;
+        // combines all these values together while keeping their vertical velocity;
+        //var move = forward + right + new Vector3(0,rb.linearVelocity.y,0);
+        var move = forward + right;
+        var currentVelocityNoY = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+        if (isGrounded)
+        {
+            rb.AddForce(move - currentVelocityNoY, ForceMode.VelocityChange);
+        }
+        else
+        {
+            rb.AddForce(move - currentVelocityNoY, ForceMode.Acceleration);
+        }
+        Debug.Log(rb.linearVelocity.magnitude);
+    }
+
+    void HandleJump()
+    {
+        if (jump)
+        {
+            Debug.Log("adding jump force");
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z) + Vector3.up * jumpForce ; 
+            //rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jump = false;
+        }
+    }
+
+    void HandleFall()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+
+            // basically what this does is that like, we add extra gravity. if we want gravity to be normal like default unity physics,
+            // then we set fall multiplier to 1, because 1 - 1 = 0 => physics.gravity.y * 0 = 0. so no additional gravity will be added because
+            // engine already applying gravity
+            float extraDownVel = Physics.gravity.y * (fallMultiplier - 1);
+            //rb.linearVelocity += Vector3.up * (extraDownVel * Time.fixedDeltaTime);
+            rb.AddForce(Vector3.up * extraDownVel, ForceMode.Acceleration);
+            //rb.linearVelocity = new Vector3(rb.linearVelocity.x, fallRate , rb.linearVelocity.z);
         }
     }
 
